bfd470a1904374c7a0dcebe64017ff3fb8dac2ce
chandler-barlow <chandlerbrlw@gmail.com>
2025-11-23 06:06:50 +0000

    remove the slack script

--- scripts/slack-send-message.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -XPOST https://slack.com/api/chat.postMessage \
-     -H "Authorization: Bearer $SLACK_TOKEN" \
-     -d "channel=$SLACK_CHANNEL" \
-     -d "text=$MESSAGE"
