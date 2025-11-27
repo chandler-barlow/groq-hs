5b31e900ba40a09f62562e2b6c4b919bfb78af09
chandler-barlow <chandlerbrlw@gmail.com>
2025-11-27 03:44:10 +0000

    added tree fmt

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.haskell-flake.flakeModule ];
+      imports = [ 
+        inputs.haskell-flake.flakeModule 
+        inputs.treefmt-nix.flakeModule
+      ];
 
       perSystem = { self', pkgs, ... }: {
         haskellProjects.default = {
@@ -24,6 +29,26 @@
         };
 
         packages.default = self'.packages.groq-hs;
+
+        treefmt.config = {
+            # inherit (config.flake-root) projectRootFile;
+            package = pkgs.treefmt;
+            flakeFormatter = false; # For https://github.com/numtide/treefmt-nix/issues/55
+
+            programs.ormolu.enable = true;
+            programs.nixpkgs-fmt.enable = true;
+            programs.cabal-fmt.enable = true;
+            programs.hlint.enable = true;
+
+            programs.ormolu.package = pkgs.haskellPackages.fourmolu;
+            settings.formatter.ormolu = {
+              options = [
+                "--ghc-opt"
+                "-XImportQualifiedPost"
+              ];
+            };
+        };
       };
+
     };
 }
